4aae815084bdada5176df0a0eac4d1b71b201ffc
Diogo Malfatti <diimalfatti@gmail.com>
2024-11-21 13:20:33 -0300

    implementing image storage and upload

--- src/controllers/postsControllers.js
+++ src/controllers/postsControllers.js
@@ -1,6 +1,35 @@
-import { getTodosOsPosts } from "../models/postsModel.js";
+import fs from "fs";
+import { getTodosOsPosts, criarPost } from "../models/postsModel.js";
 
 export async function listarPosts(req, res) {
   const posts = await getTodosOsPosts();
   res.status(200).json(posts);
+}
+
+export async function postarNovoPost(req, res) {
+  const novoPost = req.body;
+  try {
+    const postCriado = await criarPost(novoPost);
+    res.status(200).json(postCriado);
+  } catch (erro) {
+    console.log(erro.message);
+    res.status(500).json({ Erro: "Falha na requisicão" });
+  }
+}
+
+export async function uploadImagem(req, res) {
+  const novoPost = {
+    descricao: "",
+    imgUrl: req.file.originalname,
+    alt: "",
+  };
+  try {
+    const postCriado = await criarPost(novoPost);
+    const imagemAtualizada = `uploads/${postCriado.insertedId}.png`;
+    fs.renameSync(req.file.path, imagemAtualizada);
+    res.status(200).json(postCriado);
+  } catch (erro) {
+    console.log(erro.message);
+    res.status(500).json({ Erro: "Falha na requisicão" });
+  }
 }
--- src/models/postsModel.js
+++ src/models/postsModel.js
@@ -7,3 +7,9 @@ export async function getTodosOsPosts() {
   const colecao = db.collection("posts");
   return colecao.find().toArray();
 }
+
+export async function criarPost(novoPost) {
+  const db = conexao.db("imersao-instalike");
+  const colecao = db.collection("posts");
+  return colecao.insertOne(novoPost);
+}
